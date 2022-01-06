dddd51ee5ff0b9b72f913cb6f902787e716035aa
Barnabas Asha <barnabee58@gmail.com>
2022-01-06 16:16:41 +0100

    completed all other sections

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,13 @@
     <main>
       <Hero />
       <SectionOne />
+      <MealPlans />
+      <Recipes />
+      <Opinions />
+      <Options />
+      <Extras />
+      <Community />
+      <Footer />
     </main>
   </div>
 </template>
@@ -12,6 +19,13 @@
 import Header from "./components/Header.vue";
 import Hero from "./components/Hero.vue";
 import SectionOne from "./components/SectionOne.vue";
+import MealPlans from "./components/MealPlans/MealPlans.vue";
+import Recipes from "./components/Recipes/Recipes.vue";
+import Opinions from "./components/Opinions.vue";
+import Options from "./components/Options/Options.vue";
+import Extras from "./components/Extras.vue";
+import Community from "./components/Community.vue";
+import Footer from "./components/Footer/Footer.vue";
 
 export default {
   name: "App",
@@ -19,6 +33,13 @@ export default {
     Header,
     Hero,
     SectionOne,
+    MealPlans,
+    Recipes,
+    Opinions,
+    Options,
+    Extras,
+    Community,
+    Footer,
   },
 };
 </script>
